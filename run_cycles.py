543978e4f4640a4bbdeddbd20dda7d0878324ebd
Yuning Shi <shiyuning@gmail.com>
2026-05-27 13:09:34 -0400

    :arrow_up: UPdate to Cycles v1.5.20 and Cycles-utils v4.0.2

--- run_cycles.py
+++ run_cycles.py
@@ -1,23 +1,23 @@
 import pandas as pd
 from cycles import CyclesRunner
+from pathlib import Path
 
-CYCLES = './Cycles'
+CYCLES = Path('./') / 'Cycles'
 
 YEAR_START=1980
 YEAR_END=2020
 
 
 def main():
-    simulation_name = lambda x: x['GID']
-
-    control_dict = lambda x: {
-        'simulation_start_date': YEAR_START,
-        'simulation_end_date': YEAR_END,
+    control_dict = {
+        'simulation_name': lambda x: x['GID'],
+        'simulation_start_year': YEAR_START,
+        'simulation_end_year': YEAR_END,
         'rotation_size': 1,
         'crop_file': 'Maize.crop',
-        'operation_file': f'{simulation_name(x)}.operation',
-        'soil_file': f'soil/{x["soil"]}',
-        'weather_file': f'weather/{x["weather"]}',
+        'operation_file': lambda x: f'{x["GID"]}.operation',
+        'soil_file': lambda x: f'soil/{x["soil"]}',
+        'weather_file': lambda x: f'weather/{x["weather"]}',
         'automatic_nitrogen': 1,
         'daily_weather_out': 1,
         'daily_crop_out': 1,
@@ -28,24 +28,23 @@ def main():
         'daily_soil_lyr_cn_out': 1,
     }
 
-    operation_dict = lambda x: {
-        'PD1': x['maize_plant_start'],
-        'PD2': x['maize_plant_end'],
+    operation_dict = {
+        'PD1': lambda x: x['maize_plant_start'],
+        'PD2': lambda x: x['maize_plant_end'],
         'SOIL_TEMP': 12.0,
-        'CROP': f'MaizeRM.{int(x["relative_maturity_group"])}',
+        'CROP': lambda x: f'MaizeRM.{int(x["relative_maturity_group"])}',
     }
 
     cycles_runner = CyclesRunner(
-        simulations=pd.read_csv('./simulations.csv'),
-        summary='summary.csv',
-        simulation_name=simulation_name,
-        control_dict=control_dict,
-        operation_template=f'./template/monoculture.operation',
-        operation_dict=operation_dict,
+        executable=CYCLES,
     )
 
     cycles_runner.run(
-        CYCLES,
+        simulations=pd.read_csv(Path('./simulations.csv')),
+        summary='summary.csv',
+        control_dict=control_dict,
+        operation_template=Path('./template') / 'monoculture.operation',
+        operation_dict=operation_dict,
         options='-sg',
         rm_input=False,
         silence=True,
